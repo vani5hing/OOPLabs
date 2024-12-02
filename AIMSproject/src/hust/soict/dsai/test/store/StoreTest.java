4d51ed2d57894c486d832210f38416ac80224b8d
vani5hing <lieuhoaisa2614@gmail.com>
2024-12-02 21:33:20 +0700

    lab 04

--- AIMSproject/src/hust/soict/dsai/test/store/StoreTest.java
+++ AIMSproject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -1,22 +1,22 @@
 package hust.soict.dsai.test.store;
-import hust.soict.dsai.aims.store.*;
-import hust.soict.dsai.aims.disc.*;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.store.Store;
 
 public class StoreTest {
 	public static void main(String[] args) {
-		Store store = new Store(3);
+		Store store = new Store();
 		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		store.addDVD(dvd1);
+		store.addMedia(dvd1);
 		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		store.addDVD(dvd2);
+		store.addMedia(dvd2);
 		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		store.addDVD(dvd3);
+		store.addMedia(dvd3);
 		
 		store.printItemsInStore();
-		store.removeDVD(dvd2);
+		store.addMedia(dvd2);
 		store.printItemsInStore();
  	}
 }
